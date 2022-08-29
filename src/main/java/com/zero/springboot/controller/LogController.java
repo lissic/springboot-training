7aded3a2bb643837c63c0bef8a1f523d8b94df66
zero <190998853@qq.com>
2022-08-29 10:44:52 +0800

    测试springboot的日志框架

---
+++ src/main/java/com/zero/springboot/controller/LogController.java
@@ -0,0 +1,34 @@
+package com.zero.springboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * @author zero
+ * @description LogController
+ * @date 2022/8/29 9:59
+ */
+@Slf4j
+@RestController
+public class LogController {
+    // Logger logger = LoggerFactory.getLogger(this.getClass());
+    @GetMapping("/log")
+    public Map<String, Object> log() {
+        log.trace("---------------Trace级别的日志------------");
+        log.debug("---------------Debug级别的日志------------");
+        log.info("----------------Info级别的日志-------------");
+        log.warn("----------------Warn级别的日志-------------");
+        log.error("---------------Error级别的日志------------");
+        Map<String, Object> map = new HashMap<>();
+        map.put("log", "Log");
+        return map;
+    }
+
+}
